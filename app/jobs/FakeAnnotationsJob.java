6237bdc43162e1e6ea0c9e02d0ffd9047eab77c1
loopasam <samuel.croset@gmail.com>
2014-04-15 09:59:38 +0100

    fake annotator updated

--- app/jobs/FakeAnnotationsJob.java
+++ app/jobs/FakeAnnotationsJob.java
@@ -13,18 +13,26 @@ import play.jobs.Job;
 
 public class FakeAnnotationsJob extends Job {
 
-	public static int numberOfFakeAnnotations = 10;
+	public static int numberOfFakeAnnotations = Integer.parseInt((String) play.Play.configuration.get("application.fakeAnnotations"));
 
 	public void doJob(){
 		Stopwatch stopwatch = Stopwatch.createUnstarted();
 		stopwatch.start();
 
+		int counterFlush = 0;
+
 		for (int i = 1; i <= numberOfFakeAnnotations; i++) {
 			Logger.info("Fake annotation: " + i + "/" + numberOfFakeAnnotations);
 			AnnotatedAssay assay = AnnotatedAssay.find("order by rand()").first();
 			assay.addFakeAnnotation();
+
+			counterFlush++;
+			if (counterFlush%50 == 0) {
+				AnnotatedAssay.em().flush();
+				AnnotatedAssay.em().clear();
+			}
 		}
-		
+
 		stopwatch.stop();
 		Utils.emailAdmin("FakeAnnotationsJob completed", "Job done in " +
 				"" + stopwatch.elapsed(TimeUnit.MINUTES) + " minutes and generating " + numberOfFakeAnnotations
