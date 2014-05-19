109ca01ca95c56f1fd7cac2a12054bdc5f651415
loopasam <samuel.croset@gmail.com>
2014-05-19 15:25:18 +0100

    semantic job modification

--- app/jobs/SemanticSimplificationJob.java
+++ app/jobs/SemanticSimplificationJob.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import com.google.common.base.Stopwatch;
+import java.util.ArrayList;
 
 import models.AnnotatedAssay;
 import models.Annotation;
+import models.BaoTerm;
 import play.Logger;
 import play.jobs.Job;
 
@@ -24,11 +26,56 @@ public class SemanticSimplificationJob extends Job {
         int total = assays.size();
         int counter = 0;
 
+        //Identify all the annotations to be removed (ids) and store them somewhere (= list)
+        //Iterate over the list, make the modifications and flush regularly
+        List<Long> toRemove = new ArrayList<Long>();
+
         for (AnnotatedAssay annotatedAssay : assays) {
             counter++;
             Logger.info("Assay " + counter + "/" + total);
 
-            annotatedAssay.doSemanticSimplification();
+            if (annotatedAssay.annotations.size() > 1) {
+
+                List<BaoTerm> annotatedTerms = new ArrayList<BaoTerm>();
+
+                for (Annotation annotation : annotatedAssay.annotations) {
+                    //Retrieves all the annotated terms not to be removed
+                    if (!annotation.toRemove) {
+                        annotatedTerms.add(annotation.term);
+                    }
+                }
+
+                for (Annotation annotation : annotatedAssay.annotations) {
+                    if (!annotation.toRemove) {
+                        //If the children of an annotated term are
+                        //present in the annotated terms, then delete the annotation
+                        List<BaoTerm> children = annotation.term.children;
+                        for (BaoTerm child : children) {
+                            if (annotatedTerms.contains(child)) {
+                                //The annotation should be removed - flag as such to avoid concurrency problems
+                                toRemove.add(annotation.id);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        Logger.info("Flaging annotations to be removed: " + toRemove.size());
+        int counterToRemove = 0;
+        int totalToRemove = toRemove.size();
+
+        for (Long id : toRemove) {
+            counterToRemove++;
+            Logger.info("Removing: " + counterToRemove + "/" + totalToRemove);
+            Annotation annotation = Annotation.findById(id);
+            annotation.toRemove = true;
+            annotation.save();
+            if (counterToRemove % 100 == 0) {
+                Annotation.em().flush();
+                Annotation.em().clear();
+            }
         }
 
         stopwatch.stop();
